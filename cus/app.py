ef76fb065970950fa746898780155d2dcf91e9be
fushisanlang <13@fushisanlang.cn>
2023-12-11 15:39:28 +0800

    update cus

--- cus/app.py
+++ cus/app.py
@@ -1,21 +1,31 @@
-from flask import Flask, request
+from flask import Flask, request,render_template
 import sqlite3
 from callwx import callwx
 app = Flask(__name__)
 
-
+##########################
+#CREATE TABLE IF NOT EXISTS userWait (
+#    id INTEGER PRIMARY KEY AUTOINCREMENT,
+#    email TEXT NOT NULL,
+#    wechat TEXT NOT NULL,
+#    done INTEGER DEFAULT 0
+#);
+##########################
 # API 端点，接收数据并插入到数据库
+@app.route('/')
+def root():
+    return render_template('index.html')
 @app.route('/insert', methods=['POST'])
 def insert_data():
     data = request.json  # 从 POST 请求中获取 JSON 数据
-    name = data.get('name')
+    email = data.get('email')
     wechat = data.get('wechat')
     conn = sqlite3.connect('/app/data/database.db')
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO userWait (name, wechat) VALUES (?, ?)', (name, wechat))
+    cursor.execute('INSERT INTO userWait (email, wechat) VALUES (?, ?)', (email, wechat))
     conn.commit()
     conn.close()
-    callwx(name, wechat)
+    callwx(email, wechat)
     return 'ok' 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",  debug=True)  # 启动 Flask 应用
--- cus/callwx.py
+++ cus/callwx.py
@@ -1,6 +1,6 @@
 import requests
 import json
-def callwx(name,wechat) :
+def callwx(email,wechat) :
     url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=707c57bf-abcd-4325-b7d6-c7dffcc54fb5" 
 
     headers = {
@@ -11,7 +11,7 @@ def callwx(name,wechat) :
         "msgtype": "markdown",
         "markdown": {
             "content": f"<font color=\"warning\">有新注册信息，请相关同事注意。</font>\n"
-                       f">用户:<font color=\"comment\">  {name}</font>\n"
+                       f">邮箱:<font color=\"comment\">  {email}</font>\n"
                        f">微信:<font color=\"comment\">  {wechat}</font>\n"
         }
     }
